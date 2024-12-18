b6227a924fa96faa1acf3def7f80768f85eca133
Rahul <h.raul0905@gmail.com>
2024-12-18 20:28:00 +0530

    changes in skills css and resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
 
                     <motion.a
                         download=""
-                        href="https://drive.google.com/file/d/1-m4odk1NC92YUEErpul1MKW625XO1vSc/view?usp=drive_link"
+                        href="https://drive.google.com/file/d/1HD7LXOziLntdlsR6TupRgGx6pOkJQRdw/view?usp=drive_link"
                         className="button button--flex"
                         whileHover={{ scale: 1.1 }}
                         whileTap={{ scale: 1.0 }}
